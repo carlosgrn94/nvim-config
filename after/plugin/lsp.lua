615cf0f46ba790ba6635664f5623fc4a2445bc30
Carlos Rivera <river0115@algonquinlive.com>
2023-10-26 22:11:09 -0400

    added tab to select option from autocomplet

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,6 @@
 local lsp_zero = require('lsp-zero')
 
-function on_attach(client, bufnr) 
+function on_attach(_, bufnr)
 	local opts = {buffer = bufnr, remap = false}
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 end
@@ -24,7 +24,18 @@ require('mason-lspconfig').setup({
 })
 
 local cmp = require('cmp')
+local luasnip = require('luasnip')
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup({
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body)
+      end,
+    },
 	sources = {
 		{name = 'path'},
 		{name = 'nvim_lsp'},
@@ -32,6 +43,26 @@ cmp.setup({
 	},
 	formatting = lsp_zero.cmp_format(),
 	mapping = cmp.mapping.preset.insert({
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 		-- ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 		-- ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 		-- ['<C-Space>'] = cmp.mapping.complete(),
@@ -48,7 +79,7 @@ cmp.setup({
 		-- Scroll up and down in the completion documentation
 		['<C-u>'] = cmp.mapping.scroll_docs(-4),
 		['<C-d>'] = cmp.mapping.scroll_docs(4),
-	})
+	}),
 })
 
 local nvim_lsp = require('lspconfig')
