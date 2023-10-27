a1b358240b6a6e3699e9a78792513bea3ac24b47
Carlos Rivera <river0115@algonquinlive.com>
2023-10-26 22:13:07 -0400

    cleanup

--- lua/chuck/packer.lua
+++ lua/chuck/packer.lua
@@ -7,19 +7,8 @@ return require('packer').startup(function(use)
 
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.4',
-		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
-
-	--[[
-	use({
-		'rose-pine/neovim',
-		as = 'rose-pine',
-		config = function()
-			vim.cmd('colorscheme rose-pine')
-		end
-	})
-	]]
 
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
